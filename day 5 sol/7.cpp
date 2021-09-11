7931cde421c7f21629c9c86f441e3478d103e8b3
viraj <virajshitole2001@gmail.com>
2021-09-11 18:41:38 +0530

    day 5 solution

--- day 5 sol/7.cpp
+++ day 5 sol/7.cpp
@@ -1,12 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+// 1 
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
 int main(){
     int n;
     cin>>n;
-    for(int i = 1 ;i<n;i++){
-        for(int j= 1 ;j<n;j++){
-            cout<<" * "<<"  ";
+    for(int i = 1 ;i<=n;i++){
+        for(int j= 1 ;j<=i;j++){
+            cout<<j<<" ";
 
+            
         }
         cout<<endl;
     }

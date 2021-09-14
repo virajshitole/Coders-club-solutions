d581fb52981d8df535b7a028daa376d0920dad1d
viraj <virajshitole2001@gmail.com>
2021-09-14 22:30:59 +0530

    sol

---
+++ day 6 sol/1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Isprime(int a,int b){
+    while(a<=b){
+        // bk
+        
+
+    }
+}
+    
+int main(){
+    //1) Write a Program to Display Prime Numbers Between Two Intervals (entered by the user) Using Functions									
+    int a,b;
+    cin>>a>>b;
+    Isprime(a,b);
+    return 0;
+}
---
+++ day 6 sol/3.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //3) Write a Program to Convert Binary Number to Decimal manually by creating user-defined functions.									
+    
+    return 0;
+}
